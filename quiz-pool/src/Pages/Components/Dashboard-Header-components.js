6e7cfeb386ee26f710b7537fb1e4623653d54a9e
ajaywagh_2298 <ajaywagh2298@gmail.com>
2023-05-27 00:30:45 +0530

    commit

--- quiz-pool/src/Pages/Components/Dashboard-Header-components.js
+++ quiz-pool/src/Pages/Components/Dashboard-Header-components.js
@@ -4,17 +4,24 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import "../../CSS/HeaderComponents.css";
 
 const DashboardHeaderComponents =()=> {
     return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        News
+        <Box sx={{flexGrow: 1}} >
+            <AppBar position="static" >
+                <Toolbar className={"bar"}>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}} className={"nav-button"}>
+                        <img
+                            src={`https://almablog-media.s3.ap-south-1.amazonaws.com/Group_1000002112_7184f3d26d.png`}
+                            srcSet={`https://almablog-media.s3.ap-south-1.amazonaws.com/Group_1000002112_7184f3d26d.png 2x`}
+                            alt={'logo'}
+                            loading="lazy"
+                            className={"logo"}
+                      />
                     </Typography>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">My Quiz</Button>
+                    <Button ><Typography className={"nav-button"}>Home</Typography></Button>
+                    <Button ><Typography className={"nav-button"}>My Quiz</Typography></Button>
                 </Toolbar>
             </AppBar>
         </Box>
--- quiz-pool/src/Pages/Dashboard-Page.js
+++ quiz-pool/src/Pages/Dashboard-Page.js
@@ -1,10 +1,29 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import DashboardHeaderComponents from "./Components/Dashboard-Header-components";
+import Typography from "@mui/material/Typography";
 
 const DashboardPage = () => {
     return (
         <div>
             <DashboardHeaderComponents/>
-            <h1>Dashboard</h1>
+            <CssBaseline/>
+            <Container maxWidth="lm">
+                <Box className={'card mt-lg-5 border-0 shadow-none justify-content-center text-center'} >
+                   <Box className={'card-bod d-flex'}>
+                       <Typography variant="h1" component="div" sx={{flexGrow: 1}} className={"nav-button"}>
+                       AJay
+                   </Typography>
+                    <Typography variant="h1" component="div" sx={{flexGrow: 1}} className={"nav-button"}>
+                       AJay
+                   </Typography>
+                    <Typography variant="h1" component="div" sx={{flexGrow: 1}} className={"nav-button"}>
+                       AJay
+                   </Typography>
+                   </Box>
+                </Box>
+            </Container>
         </div>
     )
 }
